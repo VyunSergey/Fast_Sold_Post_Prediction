5d4d83019b4e9a3542846bb1b7f4b341ccc4d320
VyunSergey <vyun.sergey@icloud.com>
2019-06-02 00:53:51 +0300

    optimise tree params grid

--- model/classifiers/decision_tree.py
+++ model/classifiers/decision_tree.py
@@ -4,9 +4,9 @@ tree_name = 'DecisionTreeClassifier'
 
 tree_params_grid = {
     'max_depth': [None, 10, 100],
-    'max_features': [None, 100, 1000, 100000],
+    'max_features': [None, 1000, 100000],
     'max_leaf_nodes': [None, 3, 5],
-    'min_samples_split': [2, 3, 5]
+    'min_samples_split': [2, 3]
 }
 
 tree = DecisionTreeClassifier(random_state=42)
