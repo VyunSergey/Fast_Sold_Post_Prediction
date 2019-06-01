14aac5167e59ab02008d6af1620965dca4cced32
VyunSergey <vyun.sergey@icloud.com>
2019-06-02 00:34:16 +0300

    add DecisionTreeClassifier

---
+++ model/classifiers/decision_tree.py
@@ -0,0 +1,15 @@
+from sklearn.tree import DecisionTreeClassifier
+
+tree_name = 'DecisionTreeClassifier'
+
+tree_params_grid = {
+    'max_depth': [None, 10, 100],
+    'max_features': [None, 100, 1000, 100000],
+    'max_leaf_nodes': [None, 3, 5],
+    'min_samples_split': [2, 3, 5]
+}
+
+tree = DecisionTreeClassifier(random_state=42)
+
+if __name__ == "__main__":
+    print(tree.get_params())
