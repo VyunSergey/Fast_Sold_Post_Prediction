7c8775240c669464535f22f4d6556746c8f72f88
VyunSergey <vyun.sergey@icloud.com>
2019-06-02 00:32:32 +0300

    fix #

--- model/classifiers/linear.py
+++ model/classifiers/linear.py
@@ -5,7 +5,7 @@ linear_name = 'SGDClassifier'
 linear_params_grid = {
     'alpha': [0.0001],
     'loss': ['log'],
-    #'loss': ['hinge', 'log'],
+    # 'loss': ['hinge', 'log'],
     'max_iter': [1000],
     'tol': [0.001, 0.0001, 0.01, 0.1]
 }
